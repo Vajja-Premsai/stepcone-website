51b4242dff14803ca0705d6a145dcb7b395181bf
premsai143 <108271437+premsai143@users.noreply.github.com>
2022-10-16 02:16:10 +0530

    Create accommodation.php

---
+++ master/accommodation.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Stepcone 2023</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+
+	<!-- CSS here -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/slicknav.css">
+    <!-- <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/gijgo.css"> -->
+	<link rel="stylesheet" href="assets/css/animate.min.css">
+	<!-- <link rel="stylesheet" href="assets/css/magnific-popup.css"> -->
+	<link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="assets/css/themify-icons.css">
+	<link rel="stylesheet" href="assets/css/slick.css">
+	<!-- <link rel="stylesheet" href="assets/css/nice-select.css"> -->
+	<link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- <link rel="stylesheet" href="assets/css/home.css"> -->
+    <link rel="stylesheet" href="assets/css/no-animate.css">
+</head>
+<body>
+    <!-- ? Preloader Start -
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/loder.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader Start -->
+    <?php
+    include 'header.php';
+     ?>
+<main>
+   
+    <div class="main-body-section acc">
+        <section class="latest-news news-detail padding-top-50 margin-bottom-50">
+            <div class="container">
+                <div class="row"  >
+                    <div class="heading-block  animated zoomIn" style="text-align:center; padding:40px;">
+                        <h1 style="letter-spacing:2px;">ACCOMMODATION</h1>
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="col-lg-4 col-md-4 col-sm-6 animated fadeInLeftBig">
+                            <img class="img-responsive eve-img" src="assets/img/accc.jpg" width=100%;>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-sm-6 desc animated fadeInRightBig">
+                        <div class="news-detail margin-bottom-30">
+                            <h2 style="margin-top:-20px;text-align:center;">Description</h2>
+                            <ul class="hos"  style="font-size:24px;">
+                                <li>Along with stepcone kit two day lunch will be provided for the persons who have undergone basic registration for STEPCONE 2023.</li>
+                                <li>For the persons who have opted accommodation,the following will be provided:
+                                    <ul class="hos" >
+                                        <li>Stepcone kit</li>
+                                        <li>Break-fast,lunch,dinner for two days</li>
+                                        <li>Bed</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <section class="contact-info ">
+            <div class="container">
+                <ul class="row">
+                    <li class="col-sm-6 animated zoomIn"> <i class="fa fa-phone phone" style="font-size:50px;color:#f6c72e;"></i>
+                        <h4 class="font-alegreya">CONTACT</h4>
+                        <p>aaaaaaaaaa  <a href="tel:+91-709-392-2589">+91 99999 99999</a></p>
+                        <p>aaaaaaaaaaa <a href="tel:+91-944-131-4938">+91 99999 99999</a></p>
+                    </li>
+                    <li class="col-sm-6 animated zoomIn"> <i class="fa fa-credit-card reg" style="font-size:50px;color:#f6c72e;"></i>
+                        <h4 class="font-alegreya">REGISTRATION</h4>
+                        <a href="registration.html">REGISTER NOW</a>
+                        <p></p>
+                    </li>
+
+                </ul>
+
+            </div>
+        </section>
+</main>
+    <?php
+    include 'footer.php';
+    ?>
+    <!-- Scroll Up -->
+    <div id="back-top" >
+        <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+    </div>
+
+    <!-- JS here -->
+
+    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="./assets/js/owl.carousel.min.js"></script>
+    <script src="./assets/js/slick.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="./assets/js/wow.min.js"></script>
+    <script src="./assets/js/animated.headline.js"></script>
+    <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Date Picker -->
+    <script src="./assets/js/gijgo.min.js"></script>
+    <!-- Nice-select, sticky -->
+    <script src="./assets/js/jquery.nice-select.min.js"></script>
+    <script src="./assets/js/jquery.sticky.js"></script>
+    
+    <!-- counter , waypoint -->
+    <script src="./assets/js/jquery.counterup.min.js"></script>
+    <script src="./assets/js/waypoints.min.js"></script>
+    <script src="./assets/js/jquery.countdown.min.js"></script>
+    <!-- contact js -->
+    <script src="./assets/js/contact.js"></script>
+    <script src="./assets/js/jquery.form.js"></script>
+    <script src="./assets/js/jquery.validate.min.js"></script>
+    <script src="./assets/js/mail-script.js"></script>
+    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+    
+    <!-- Jquery Plugins, main Jquery -->	
+    <script src="./assets/js/plugins.js"></script>
+    <script src="./assets/js/main.js"></script>
+    
+    </body>
+</html>
