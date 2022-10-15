823fa7bb5711366ba55c0dc0237678eb5cc8c5be
premsai143 <108271437+premsai143@users.noreply.github.com>
2022-10-16 02:20:58 +0530

    master

---
+++ master/contact.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Stepcone 2023</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+
+	<!-- CSS here -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/slicknav.css">
+    <!-- <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/gijgo.css"> -->
+	<link rel="stylesheet" href="assets/css/animate.min.css">
+	<!-- <link rel="stylesheet" href="assets/css/magnific-popup.css"> -->
+	<link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="assets/css/themify-icons.css">
+	<link rel="stylesheet" href="assets/css/slick.css">
+	<!-- <link rel="stylesheet" href="assets/css/nice-select.css"> -->
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <?php
+    include 'header.php';
+     ?>
+
+    <!--social media contact-->
+    <div class="customer-reviews-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Social Media</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row social-contact">
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <a href="https://www.facebook.com/stepcone2023" target="_blank"><i class="fab fa-facebook-f" ></i></a>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <a href="https://www.instagram.com/stepcone_2023_official/" target="_blank"><i class="fab fa-twitter"></i></a>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <a href="https://twitter.com/Stepcone_2023" target="_blank"><i class="fab fa-instagram"></i></a>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <a href="https://www.linkedin.com/company/stepcone-2023/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+			</div>
+		</div>
+	</div>
+    <!-- Start Customer Reviews -->
+	<div class="customer-reviews-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Contact Info</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+                            <div class="col-lg-3 col-md-4 col-sm-6 media text-center active">
+								<div class="img-box p-1 border rounded-circle m-auto">
+									<img class="d-block w-100 rounded-circle" src="assets/img/gallery/men.jpg" alt="">
+								    <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Paul Mitchel</strong></h5>
+								    <h6 class="text-dark m-0">Computer Science & Engineering <br> <a href="#">Phone : 99999 99999</a></h6>
+                                </div>
+							</div>
+							<div class="col-lg-3 col-md-4 col-sm-6 media text-center">
+								<div class="img-box p-1 border rounded-circle m-auto">
+									<img class="d-block w-100 rounded-circle" src="assets/img/gallery/men.jpg" alt="">
+								    <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Steve Fonsi</strong></h5>
+								    <h6 class="text-dark m-0">Information Technology <br> <a href="#">Phone : 99999 99999</a></h6>
+                                </div>
+							</div>
+							<div class="col-lg-3 col-md-4 col-sm-6 media text-center">
+								<div class="img-box p-1 border rounded-circle m-auto">
+									<img class="d-block w-100 rounded-circle" src="assets/img/gallery/men.jpg" alt="">
+    								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Daniel vebar</strong></h5>
+	    							<h6 class="text-dark m-0">Mechanical Engineering <br> <a href="#">Phone : 99999 99999</a></h6>
+                                </div>
+							</div>
+                            <div class="col-lg-3 col-md-4 col-sm-6 media text-center">
+								<div class="img-box p-1 border rounded-circle m-auto">
+									<img class="d-block w-100 rounded-circle" src="assets/img/gallery/men.jpg" alt="">
+								    <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">John</strong></h5>
+								    <h6 class="text-dark m-0">Computer Science & Engineering <br> <a href="#">Phone : 99999 99999</a></h6>
+                                </div>
+							</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Customer Reviews -->
+	
+    <!-- Start Contact -->
+	<div class="contact-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6">
+					<form id="contactForm">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									<input type="text" class="form-control" id="name" name="name" placeholder="Your Name" required data-error="Please enter your name">
+									<div class="help-block with-errors"></div>
+								</div>                                 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<input type="text" placeholder="Your Email" id="email" class="form-control" name="name" required data-error="Please enter your email">
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group"> 
+									<textarea class="form-control" id="message" placeholder="Your Message" rows="4" data-error="Write your message" required></textarea>
+									<div class="help-block with-errors"></div>
+								</div>
+								<div class="submit-button text-center" style="margin-top: 20%;">
+									<button class="btn btn-common" id="submit" type="submit">Send Message</button>
+									<div id="msgSubmit" class="h3 text-center hidden"></div> 
+									<div class="clearfix"></div> 
+								</div>
+							</div>
+						</div>            
+					</form>
+				</div>
+				<div class="col-lg-6 col-md-6">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4010.6929224241453!2d83.66036479054259!3d18.466096716939866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3c772724754cc9%3A0xcf876cd74c2b6b01!2sGMR%20Institute%20of%20Technology!5e0!3m2!1sen!2sin!4v1665809294194!5m2!1sen!2sin" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact -->
+	<?php
+    include 'footer.php';
+     ?>
+    <!-- Contact info -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<!-- <script src="js/popper.min.js"></script> -->
+	<script src="js/bootstrap.min.js"></script>
+    
+</body>
+</html>
